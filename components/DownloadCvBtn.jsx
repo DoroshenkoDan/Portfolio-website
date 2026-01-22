be97b327391e6733d0eb33914d5c7b8f5a9a2f85
Daniil Doroshenko <doroshenkowin@gmail.com>
2026-01-22 17:59:34 +0100

    Change CV

--- components/DownloadCvBtn.jsx
+++ components/DownloadCvBtn.jsx
@@ -1,19 +1,15 @@
 "use client"
-import { useTranslations, useLocale } from 'next-intl'
+import { useTranslations } from 'next-intl'
 import { Button } from "@/components/ui/button"
 import { FiDownload } from "react-icons/fi"
 
 const DownloadCvBtn = () => {
 	const t = useTranslations('main')
-	const locale = useLocale()
 
 	const handleDownloadCV = () => {
 		const link = document.createElement('a')
-		const cvFile = locale === 'uk' ? '/CvUA.pdf' : '/CvEn.pdf'
-		const fileName = locale === 'uk' ? 'Daniil Doroshenko CV-UA.pdf' : 'Daniil Doroshenko CV-EN.pdf'
-
-		link.href = cvFile
-		link.download = fileName
+		link.href = '/Doroshenko Daniil CV-En.pdf'
+		link.download = 'Dorooshenko Daniil CV-En.pdf'
 		document.body.appendChild(link)
 		link.click()
 		document.body.removeChild(link)
